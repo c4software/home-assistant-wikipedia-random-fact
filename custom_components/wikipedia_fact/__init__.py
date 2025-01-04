bb31ab4acd857ee7a72fe7615f35399d3f53918c
Valentin Brosseau <c4software@gmail.com>
2025-01-04 15:57:10 +0100

    Update

--- custom_components/wikipedia_fact/__init__.py
+++ custom_components/wikipedia_fact/__init__.py
@@ -1,22 +1,21 @@
-"""Initialisation de l'intégration Wikipedia Fact."""
-from homeassistant.core import HomeAssistant
+"""The Wikipedia Random Fact integration."""
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+import voluptuous as vol
+import homeassistant.helpers.config_validation as cv
 
 DOMAIN = "wikipedia_fact"
+PLATFORMS: list[Platform] = [Platform.SENSOR]
 
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Configurer l'intégration Wikipedia Fact via YAML (si nécessaire)."""
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
+async def async_setup(hass: HomeAssistant, config: dict) -> bool:
     return True
 
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Configurer une entrée créée depuis l'interface utilisateur."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Décharger une entrée."""
-    await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    return True
-
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    return await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
